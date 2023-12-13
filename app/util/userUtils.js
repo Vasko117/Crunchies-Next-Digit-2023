c731d43ef7aa181a20dd60ab256d41efd98896d8
DimitrijKrstev <antishadowss@gmail.com>
2023-12-13 15:28:10 +0100

    login done + menu item images test

--- app/util/userUtils.js
+++ app/util/userUtils.js
@@ -1,25 +1,30 @@
-export const loginUser = (username, password, setUser) => {
-	fetch("http://localhost:8008/user/login", {
-		method: "POST",
-		body: JSON.stringify({
-			username: username,
-			password: password,
-		}),
-		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-		},
-	})
-		.then((response) => {
-			if (!response.ok) throw new Error("Invalid username or password");
-			return response.json();
+export function loginUser(username, password, setUser, setError) {
+	try {
+		fetch("http://localhost:8008/user/login", {
+			method: "POST",
+			body: JSON.stringify({
+				username: username,
+				password: password,
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
 		})
-		.then((data) => {
-			setUser(data);
-		})
-		.catch((err) => {
-			throw new Error(err);
-		});
-};
+			.then((response) => {
+				if (!response.ok) throw new Error("Invalid username or password");
+				return response.json();
+			})
+			.then((data) => {
+				setUser(data);
+				setError();
+			})
+			.catch((err) => {
+				setError(err);
+			});
+	} catch (err) {
+		setError(err);
+	}
+}
 
 export const registerUser = (username, password, setUser) => {
 	fetch("http://localhost:8008/user/register", {

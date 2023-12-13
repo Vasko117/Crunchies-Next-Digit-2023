8b558048c9123432d973232536d36099b9669e04
Kristijan Panchevski <kpancevski19@gmail.com>
2023-12-13 14:24:00 +0100

    minor commit

--- app/menu/page.js
+++ app/menu/page.js
@@ -6,6 +6,7 @@ import burger from "../../public/burger(1).png";
 import combo from "../../public/burger.png";
 import soda from "../../public/soda.png";
 import wrap from "../../public/burrito.png";
+import Spline from "@splinetool/react-spline";
 import { getItems } from "../util/menuItemUtils";
 
 export default function Menu() {
@@ -44,9 +45,23 @@ export default function Menu() {
           <Image className="h-[100px] w-[100px] mt-[1vh]" src={soda}></Image>
         </button>
       </div>
-      <div className="flex justify-center md:flex-col lg:flex-row lg:items-start sm:items-center md:items-center mt-[12vh] bg-slate-100 w-3/5 h-full rounded-3xl opacity-25">
-        <input type="search" className=" bg-white w-1/3 h-5px"></input>
+      <div className="flex justify-center md:flex-col lg:flex-row lg:items-start sm:items-center md:items-center mt-[12vh] bg-slate-100 w-3/5 h-full rounded-3xl bg-opacity-25">
+        <div className="flex justify-between w-11/12">
+          <div className="text-center text-white text-[30px] font-bold font-['Rowdies'] leading-[5rem] drop-shadow-md>Choose your special bite!">
+            Choose your special bite!
+          </div>
+          <input
+            type="search"
+            placeholder="search..."
+            className=" bg-white w-1/3 h-[3vh] mt-[3vh] rounded-3xl opacity-90 right-4 text-black"
+          ></input>
+        </div>
       </div>
+      <Spline
+        id="burgerSpline"
+        className="absolute top-[2vh] right-[-11vw] scale-50"
+        scene="https://prod.spline.design/s74uL4LAplf83nO2/scene.splinecode"
+      />
     </div>
   );
 }

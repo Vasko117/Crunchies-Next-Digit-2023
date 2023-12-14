b8590d2bfabaae8b7789056c9b97f6fbc870c1c0
Kristijan Panchevski <kpancevski19@gmail.com>
2023-12-14 13:47:17 +0100

    Menu update

--- app/menu/page.js
+++ app/menu/page.js
@@ -1,77 +1,77 @@
-'use client';
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import fries from '../../public/french-fries.png';
-import burger from '../../public/burger(1).png';
-import combo from '../../public/burger.png';
-import soda from '../../public/soda.png';
-import wrap from '../../public/burrito.png';
-import Spline from '@splinetool/react-spline';
-import { getItems } from '../util/menuItemUtils';
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import fries from "../../public/french-fries.png";
+import burger from "../../public/burger(1).png";
+import combo from "../../public/burger.png";
+import soda from "../../public/soda.png";
+import wrap from "../../public/burrito.png";
+import Spline from "@splinetool/react-spline";
+import { getItems } from "../util/menuItemUtils";
 
 export default function Menu() {
-	const [foodItems, setFood] = useState([]);
-	const [error, setError] = useState();
+  const [foodItems, setFood] = useState([]);
+  const [error, setError] = useState();
 
-	useEffect(() => {
-		getItems(setFood);
-	}, []);
+  useEffect(() => {
+    getItems(setFood);
+  }, []);
 
-	return (
-		<div className="w-screen h-screen bg-gradient-to-t from-[#FFD600] to-[#ff5100]  inset-0 z-[-10] absolute flex justify-normal pl-[3vw]">
-			<div className="flex justify-center md:flex-col lg:flex-col lg:items-start mt-[12vh] bg-slate-100 w-max rounded-3xl opacity-25 mr-[5vw] h-max">
-				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-					<Image className="h-[10vh] w-[5vw] mt-[1vh]" src={combo}></Image>
-				</button>
-				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-					<Image className="h-[10vh] w-[5vw] mt-[1vh]" src={burger}></Image>
-				</button>
-				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-					<Image className="h-[10vh] w-[5vw] mt-[1vh]" src={fries}></Image>
-				</button>
-				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-					<Image className="h-[10vh] w-[5vw] mt-[1vh]" src={wrap}></Image>
-				</button>
-				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-					<Image className="h-[10vh] w-[5vw] mt-[1vh]" src={soda}></Image>
-				</button>
-			</div>
-			<div className=" md:flex-col lg:flex-row lg:items-start sm:items-center md:items-center mt-[12vh] bg-slate-100 w-3/5 h-full rounded-3xl bg-opacity-25">
-				<div className="flex justify-between w-full">
-					<div className="text-center text-white text-[30px] font-bold font-['Rowdies'] leading-[5rem] drop-shadow-md m-[1vw] ml-[2vw]">
-						Choose your special bite!
-					</div>
-					<input
-						type="search"
-						placeholder="search..."
-						className=" bg-white w-1/3 h-[3vh] mt-[4.5vh] rounded-3xl opacity-90 mr-[4vh] text-black "
-					></input>
-				</div>
-				<div className="flex justify-normal font-['Rowdies'] text-white">
-					{foodItems.map((food) => (
-						<div
-							key={food.id}
-							className="bg-white rounded-md w-1/6 h-1/6 bg-opacity-20 m-[3vh] text-center"
-						>
-							<p>{food.name}</p>
-							<p>{food.price}</p>
-							{food.id == 2 && (
-								<Image
-									width="100"
-									height="100"
-									src={'/' + food.id + '.png'}
-								></Image>
-							)}
-						</div>
-					))}
-				</div>
-			</div>
+  return (
+    <div className="w-screen h-screen bg-gradient-to-t from-[#FFD600] to-[#ff5100]  inset-0 z-[-10] absolute flex justify-normal pl-[3vw]">
+      <div className="flex justify-center md:flex-col lg:flex-col lg:items-start mt-[12vh] bg-slate-100 w-max rounded-3xl opacity-25 mr-[5vw] h-max">
+        <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
+          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={combo}></Image>
+        </button>
+        <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
+          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={burger}></Image>
+        </button>
+        <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
+          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={fries}></Image>
+        </button>
+        <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
+          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={wrap}></Image>
+        </button>
+        <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
+          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={soda}></Image>
+        </button>
+      </div>
+      <div className=" md:flex-col lg:flex-row lg:items-start sm:items-center md:items-center mt-[12vh] bg-slate-100 w-3/5 h-full rounded-3xl bg-opacity-25">
+        <div className="flex justify-between w-full">
+          <div className="text-center text-white text-[30px] font-bold font-['Rowdies'] leading-[5rem] drop-shadow-md m-[1vw] ml-[2vw]">
+            Choose your special bite!
+          </div>
+          <input
+            type="search"
+            placeholder="search..."
+            className=" bg-white w-1/3 h-[3vh] mt-[4.5vh] rounded-3xl opacity-90 mr-[4vh] text-black "
+          ></input>
+        </div>
+        <div className="flex justify-normal font-['Rowdies'] text-white">
+          {foodItems.map((food) => (
+            <div
+              key={food.id}
+              className="bg-white rounded-md w-1/6 h-1/6 bg-opacity-20 m-[3vh] text-center"
+            >
+              <p>{food.name}</p>
+              <p>{food.price}</p>
+              {food.id == 2 && (
+                <Image
+                  width="100"
+                  height="100"
+                  src={"/" + food.id + ".png"}
+                ></Image>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
 
-			<Spline
-				id="burgerSpline"
-				className="absolute top-[2vh] right-[-11vw] scale-50"
-				scene="https://prod.spline.design/s74uL4LAplf83nO2/scene.splinecode"
-			/>
-		</div>
-	);
+      <Spline
+        id="burgerSpline"
+        className="absolute top-[2vh] right-[-11vw] scale-50"
+        scene="https://prod.spline.design/s74uL4LAplf83nO2/scene.splinecode"
+      />
+    </div>
+  );
 }
--- backend/Crunchies/src/main/java/digit/nextjs/crunchies/bootstrap/DataHolder.java
+++ backend/Crunchies/src/main/java/digit/nextjs/crunchies/bootstrap/DataHolder.java
@@ -19,12 +19,12 @@ public class DataHolder {
     @PostConstruct
     public void init(){
         menuItemRepository.saveAll(Arrays.asList(
-                MenuItem.builder().name("Burger").build(),
-                MenuItem.builder().name("Cheese Burger").build(),
-                MenuItem.builder().name("Bacon Burger").build(),
-                MenuItem.builder().name("Chicken Burger").build(),
-                MenuItem.builder().name("Extra Crispy Chicken Burger").build(),
-                MenuItem.builder().name("Crunchy Burger").build()));
+                MenuItem.builder().name("Burger").imageURL("https://cdn.discordapp.com/attachments/702177541582094394/1184805592192266301/elementor-placeholder-image.png?ex=658d4f31&is=657ada31&hm=4ed9b9c6b8b099c357e259f947e9d5bd3eb40c393fa427edda7092437999fca6&").build(),
+                MenuItem.builder().name("Cheese Burger").imageURL("https://cdn.discordapp.com/attachments/702177541582094394/1184805592192266301/elementor-placeholder-image.png?ex=658d4f31&is=657ada31&hm=4ed9b9c6b8b099c357e259f947e9d5bd3eb40c393fa427edda7092437999fca6&").build(),
+                MenuItem.builder().name("Bacon Burger").imageURL("https://cdn.discordapp.com/attachments/702177541582094394/1184805592192266301/elementor-placeholder-image.png?ex=658d4f31&is=657ada31&hm=4ed9b9c6b8b099c357e259f947e9d5bd3eb40c393fa427edda7092437999fca6&").build(),
+                MenuItem.builder().name("Chicken Burger").imageURL("https://cdn.discordapp.com/attachments/702177541582094394/1184805592192266301/elementor-placeholder-image.png?ex=658d4f31&is=657ada31&hm=4ed9b9c6b8b099c357e259f947e9d5bd3eb40c393fa427edda7092437999fca6&").build(),
+                MenuItem.builder().name("Extra Crispy Chicken Burger").imageURL("https://cdn.discordapp.com/attachments/702177541582094394/1184805592192266301/elementor-placeholder-image.png?ex=658d4f31&is=657ada31&hm=4ed9b9c6b8b099c357e259f947e9d5bd3eb40c393fa427edda7092437999fca6&").build(),
+                MenuItem.builder().name("Crunchy Burger").imageURL("https://cdn.discordapp.com/attachments/702177541582094394/1184805592192266301/elementor-placeholder-image.png?ex=658d4f31&is=657ada31&hm=4ed9b9c6b8b099c357e259f947e9d5bd3eb40c393fa427edda7092437999fca6&").build()));
 
         userRepository.saveAll(Arrays.asList(
                 User.builder().username("dimi").password("dimi").build()

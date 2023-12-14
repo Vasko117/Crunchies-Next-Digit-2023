1e756016231cc806da5831ed4cb40e79aae2469c
Kristijan Panchevski <kpancevski19@gmail.com>
2023-12-14 13:55:51 +0100

    error fix

--- app/menu/page.js
+++ app/menu/page.js
@@ -19,24 +19,24 @@ export default function Menu() {
 
   return (
     <div className="w-screen h-screen bg-gradient-to-t from-[#FFD600] to-[#ff5100]  inset-0 z-[-10] absolute flex justify-normal pl-[3vw]">
-      <div className="flex justify-center md:flex-col lg:flex-col lg:items-start mt-[12vh] bg-slate-100 w-max rounded-3xl opacity-25 mr-[5vw] h-max">
+      <div className="flex justify-center sm:flex-row md:flex-col lg:flex-col mt-[12vh] bg-slate-100 w-[220px] rounded-3xl opacity-25 mr-[5vw] h-max">
         <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={combo}></Image>
+          <Image className="h-[10vh] w-[100px] mt-[1vh]" src={combo}></Image>
         </button>
         <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={burger}></Image>
+          <Image className="h-[10vh] w-[100px]  mt-[1vh]" src={burger}></Image>
         </button>
         <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={fries}></Image>
+          <Image className="h-[10vh] w-[100px]  mt-[1vh]" src={fries}></Image>
         </button>
         <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={wrap}></Image>
+          <Image className="h-[10vh] w-[100px]  mt-[1vh]" src={wrap}></Image>
         </button>
         <button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
-          <Image className="h-[10vh] w-[5vw] mt-[1vh]" src={soda}></Image>
+          <Image className="h-[10vh] w-[100px]  mt-[1vh]" src={soda}></Image>
         </button>
       </div>
-      <div className=" md:flex-col lg:flex-row lg:items-start sm:items-center md:items-center mt-[12vh] bg-slate-100 w-3/5 h-full rounded-3xl bg-opacity-25">
+      <div className="mt-[12vh] bg-slate-100 w-3/5 h-full rounded-3xl bg-opacity-25">
         <div className="flex justify-between w-full">
           <div className="text-center text-white text-[30px] font-bold font-['Rowdies'] leading-[5rem] drop-shadow-md m-[1vw] ml-[2vw]">
             Choose your special bite!
@@ -47,21 +47,20 @@ export default function Menu() {
             className=" bg-white w-1/3 h-[3vh] mt-[4.5vh] rounded-3xl opacity-90 mr-[4vh] text-black "
           ></input>
         </div>
-        <div className="flex justify-normal font-['Rowdies'] text-white">
+        <div className=" flex flex-row font-['Rowdies'] text-white">
           {foodItems.map((food) => (
             <div
               key={food.id}
-              className="bg-white rounded-md w-1/6 h-1/6 bg-opacity-20 m-[3vh] text-center"
+              className="bg-white rounded-md w-max h-max bg-opacity-20 m-[3vh] flex flex-col justify-center items-center p-[1vh] text-center"
             >
+              <Image
+                height={100}
+                width={100}
+                src={food.imageURL}
+                className="rounded-xl"
+              ></Image>
               <p>{food.name}</p>
               <p>{food.price}</p>
-              {food.id == 2 && (
-                <Image
-                  width="100"
-                  height="100"
-                  src={"/" + food.id + ".png"}
-                ></Image>
-              )}
             </div>
           ))}
         </div>

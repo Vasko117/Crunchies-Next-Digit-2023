3995be3dbaf4387684fb19da4adea69449a7f405
DimitrijKrstev <antishadowss@gmail.com>
2023-12-14 16:13:20 +0100

    menu image crash fix

--- backend/Crunchies/src/main/java/digit/nextjs/crunchies/bootstrap/DataHolder.java
+++ backend/Crunchies/src/main/java/digit/nextjs/crunchies/bootstrap/DataHolder.java
@@ -28,10 +28,10 @@ public class DataHolder {
         MenuItem.builder().name("Double CheeseBurger").imageURL(
             "https://media.discordapp.net/attachments/1059651715865006090/1184429565532188683/dimitriuss_realistic_hamburger_066b48af-6463-4140-9565-45640633f0b1.png?ex=658bf0fe&is=65797bfe&hm=d5c137cdfc1a4a066f418e52b6757b7c798fdb34415fd0a138535e5bcfa29edd&=&format=webp&quality=lossless&width=1229&height=1229")
             .build(),
-        MenuItem.builder().name("Bacon Burger").build(),
-        MenuItem.builder().name("Chicken Burger").build(),
-        MenuItem.builder().name("Extra Crispy Chicken Burger").build(),
-        MenuItem.builder().name("Crunchy Burger").build()));
+        MenuItem.builder().name("Bacon Burger").imageURL("https://cdn.midjourney.com/7dc88a0a-c192-4eb3-95ab-93b1eeb24b4d/0_0.webp").build(),
+        MenuItem.builder().name("Chicken Burger").imageURL("https://cdn.midjourney.com/7dc88a0a-c192-4eb3-95ab-93b1eeb24b4d/0_0.webp").build(),
+        MenuItem.builder().name("Extra Crispy Chicken Burger").imageURL("https://cdn.midjourney.com/7dc88a0a-c192-4eb3-95ab-93b1eeb24b4d/0_0.webp").build(),
+        MenuItem.builder().name("Crunchy Burger").imageURL("https://cdn.midjourney.com/7dc88a0a-c192-4eb3-95ab-93b1eeb24b4d/0_0.webp").build()));
 
     userRepository.saveAll(Arrays.asList(
         User.builder().username("dimi").password("dimi").build()));
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["media.discordapp.net", "cdn.discordapp.com"],
+		domains: ["media.discordapp.net", "cdn.discordapp.com", "cdn.midjourney.com"],
 	},
 };
 

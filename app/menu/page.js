9e3768e85ca39ead28d9aebccf01673ae8bfc0e4
Vasil <vsilt2002@gmail.com>
2023-12-14 20:01:35 +0100

    Menu add

--- app/menu/page.js
+++ app/menu/page.js
@@ -8,26 +8,35 @@ import soda from '../../public/soda.png';
 import wrap from '../../public/burrito.png';
 import Spline from '@splinetool/react-spline';
 import { getItems } from '../util/menuItemUtils';
+import { relative } from 'path';
 
 export default function Menu() {
+	const [items, setItems] = useState([]);
 	const [foodItems, setFood] = useState([]);
 	const [error, setError] = useState();
-	const [itemname, setItemname] = useState('');
+	const [item, setItem] = useState('');
 
 	useEffect(() => {
 		getItems(setFood);
+		getItems(setItems);
 	}, []);
 	const handleKey = (e) => {
 		if (e.code === 'Enter') {
 			handleSearch();
 		}
 	};
-	const handleSelect = async () => {};
+	function Searching(e) {
+		setItems(
+			foodItems.filter((item) =>
+				item.name.toUpperCase().includes(e.target.value.toUpperCase())
+			)
+		);
+	}
 
 	return (
 		<div className="w-screen h-screen bg-gradient-to-t from-[#FFD600] to-[#ff5100]  inset-0 z-[-10] absolute flex justify-normal pl-[3vw] overflow-auto hide-scrollbar">
 			<div className="flex justify-center sm:flex-row md:flex-col lg:flex-col mt-[12vh] bg-slate-100 w-[220px] rounded-3xl opacity-25 mr-[5vw] h-max">
-				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
+				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center ">
 					<Image className="h-[10vh] w-[100px] mt-[1vh]" src={combo}></Image>
 				</button>
 				<button className=" bg-white rounded-3xl h-[110px] w-[180px] ml-[2vh] m-[1vh] mt-[3vh] flex justify-center">
@@ -44,69 +53,48 @@ export default function Menu() {
 				</button>
 			</div>
 			<div className="mt-[12vh] bg-slate-100 w-3/5 h-full rounded-3xl bg-opacity-25 overflow-auto hide-scrollbar">
-				<div className="flex justify-between w-full">
-					<div className="text-center text-white text-[30px] font-bold font-['Rowdies'] leading-[5rem] drop-shadow-md m-[1vw] ml-[2vw]">
+				<div className="flex justify-between w-full items-center">
+					<div className="text-center text-white text-[30px] font-bold font-['Rowdies'] leading-[5rem] drop-shadow-md mt-[1vw] ml-[2vw]">
 						Choose your special bite!
 					</div>
-					<div className="flex-col mr-3">
+					<div className="flex items-center justify-start">
 						<input
 							type="search"
 							placeholder="search..."
-							className=" bg-red-600 w-full h-[3vh] mt-[4.5vh] rounded-sm opacity-90 mr-[4vh] text-black p-2 "
-							onKeyDown={handleKey}
-							onChange={(e) => setUsername(e.target.value)}
-							value={itemname}
+							className=" bg-red-600 h-[3vh] rounded-lg opacity-90 mr-[4vh] text-black p-2 w-full "
+							onChange={Searching}
 						></input>
-						<div className="h-max border-x-2 border-y-2 border-black w-max rounded-sm">
-							<div
-								className="flex items-center p-5 cursor-pointer border-2 border-black w-max h-[10px]"
-								onClick={() => handleSelect(user)}
-							>
-								<Image src={fries} className="w-[2vw]" alt="User" />
-								<div className="font-bold text-base">
-									<span>Extra large burger</span>
-								</div>
-							</div>
-							<div
-								className="flex items-center p-5 cursor-pointer border-2 border-black w-max h-[10px] rounded-sm"
-								onClick={() => handleSelect(user)}
-							>
-								<Image src={fries} className="w-[2vw]" alt="User" />
-								<div className="font-bold text-base">
-									<span>Extra large burger</span>
-								</div>
-							</div>
-							<div
-								className="flex items-center p-5 cursor-pointer border-2 border-black w-max h-[10px] rounded-sm"
-								onClick={() => handleSelect(user)}
-							>
-								<Image src={fries} className="w-[2vw]" alt="User" />
-								<div className="font-bold text-base">
-									<span>Extra large burger</span>
-								</div>
-							</div>
-						</div>
 					</div>
 				</div>
-				<div className=" grid grid-flow-row-dense grid-cols-3 grid-rows-3 font-['Rowdies'] text-white ">
-					{foodItems.map((food) => (
+				<div className="flex flex-row flex-wrap justify-center  font-['Rowdies'] text-white overflow-auto hide-scrollbar  ">
+					{items.map((food) => (
 						<div
 							key={food.id}
-							className="bg-white rounded-md  h-max bg-opacity-20 m-[3vh] flex flex-col justify-center items-center p-[3vh] text-center rounded-2xl"
+							className=" w-[16vw] h-max flex flex-col justify-center ite w-full ms-center p-[3vh] text-center rounded-2xl drop-shadow-xl"
 						>
 							<Image
 								height={100}
 								width={100}
 								src={food.imageURL}
-								className="rounded-xl"
+								alt="food"
+								className=" h-max w-full rounded-tr-2xl rounded-tl-2xl "
 							></Image>
-							<p>{food.name}</p>
-							<p>{food.price}</p>
-							<button className="px-10 py-[15px] bg-red-600  rounded-2xl w-max scale-75 ">
-								<div className="text-center text-white text-lg font-bold font-['Rowdies'] leading-loose tracking-tight">
-									Order now!
+							<div className="flex flex-col justify-center items-center  bg-opacity-20 w-full break-words">
+								<div className="flex justify-center w-full h-max bg-orange-600 p-2 ">
+									<p>{food.name}</p>
 								</div>
-							</button>
+
+								<div className="flex flex-row w-full rounded-bl-2xl rounded-br-2xl">
+									<button className="  bg-orange-400 w-1/3 p-2  ">-</button>
+									<div className=" bg-white bg-opacity-25  w-1/3 flex items-center justify-center">
+										<p>{food.price}</p>
+									</div>
+									<button className=" bg-green-600 w-1/3 p-2 ">+</button>
+								</div>
+								<button className="bg-red-500  w-full rounded-b-2xl p-2 ">
+									Add to cart
+								</button>
+							</div>
 						</div>
 					))}
 				</div>
